3f6cf671a46e61cbb3607c4734a1620e32f6ee46
sodam2z <dltheka1014@naver.com>
2026-05-30 21:54:39 +0900

    chore: 프로젝트 생성 및 데이터베이스 연결

---
+++ src/test/java/community_board/CommunityBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package community_board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
