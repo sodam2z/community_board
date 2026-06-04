6f767967b039613cb6f30954e2c2a1b3a02e396a
sodam2z <dltheka1014@naver.com>
2026-06-04 23:54:38 +0900

    refactor: controller에 @RequiredArgsConstructor로 변경

--- src/main/java/community_board/controller/UserController.java
+++ src/main/java/community_board/controller/UserController.java
@@ -1,10 +1,11 @@
 package community_board.controller;
 
-import community_board.global.response.ApiResponse;
 import community_board.dto.UserSignupRequest;
 import community_board.dto.UserSignupResponse;
+import community_board.global.response.ApiResponse;
 import community_board.service.UserService;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,13 +14,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
+@RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
-
-    //생성자 주입
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
 
     //회원가입 API
     @PostMapping
