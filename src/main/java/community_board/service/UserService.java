393d6e0194aa527dac2466565a7d32e97294a4fb
sodam2z <dltheka1014@naver.com>
2026-06-09 22:48:30 +0900

    feat: 유저 정보 조회 구현

--- src/main/java/community_board/service/UserService.java
+++ src/main/java/community_board/service/UserService.java
@@ -1,8 +1,10 @@
 package community_board.service;
 
 import community_board.domain.User;
+import community_board.dto.user.GetUserResponse;
 import community_board.dto.user.UserSignupRequest;
 import community_board.dto.user.UserSignupResponse;
+import community_board.global.exception.CommonErrorCode;
 import community_board.global.exception.RestApiException;
 import community_board.global.exception.UserErrorCode;
 import community_board.repository.UserRepository;
@@ -13,11 +15,12 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-@Transactional(readOnly = true)
 public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
+    // 내부 로직에서 User 객체가 필요할 때 사용
+    @Transactional(readOnly = true)
     public User findById(Integer userId) {
         return userRepository.findById(userId)
                 .orElseThrow(() ->
@@ -25,6 +28,21 @@ public class UserService {
                                 UserErrorCode.USER_NOT_FOUND
                         )
                 );
+    }
+
+    // 회원 정보 조회
+    @Transactional(readOnly = true)
+    public GetUserResponse getUserInfo(Integer userId, Integer loginUserId) {
+        validateUserAccess(userId, loginUserId);
+
+        User user = findById(userId);
+        return GetUserResponse.from(user);
+    }
+
+    private void validateUserAccess(Integer userId, Integer loginUserId) {
+        if (!userId.equals(loginUserId)) {
+            throw new RestApiException(CommonErrorCode.FORBIDDEN_ACCESS);
+        }
     }
 
     //회원가입 비즈니스 로직
