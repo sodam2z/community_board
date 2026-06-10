6abe11e30d22c20b3d1273bee8ce42c99916737d
sodam2z <dltheka1014@naver.com>
2026-06-10 22:39:11 +0900

    fix: 게시물 작성,수정,삭제 권한 검사 추가

--- src/main/java/community_board/controller/PostController.java
+++ src/main/java/community_board/controller/PostController.java
@@ -8,7 +8,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
@@ -17,9 +17,10 @@ public class PostController {
     private final PostService postService;
 
     @PostMapping("/posts")
-    public ResponseEntity<?> post(@RequestBody CreatePostRequest request) {
-        Integer userId = (Integer)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        CreatePostResponse response = postService.save(userId, request);
+    public ResponseEntity<?> post(
+            @AuthenticationPrincipal Integer loginUserId,
+            @RequestBody CreatePostRequest request) {
+        CreatePostResponse response = postService.save(loginUserId, request);
         return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.of("POST_CREATED", response));
     }
 
@@ -31,14 +32,19 @@ public class PostController {
     }
 
     @PatchMapping("/posts/{postId}")
-    public ResponseEntity<?> updatePost(@PathVariable Integer postId, @RequestBody UpdatePostRequest request) {
-        UpdatePostResponse response = postService.update(postId, request);
+    public ResponseEntity<?> updatePost(
+            @PathVariable Integer postId,
+            @AuthenticationPrincipal Integer loginUserId,
+            @RequestBody UpdatePostRequest request) {
+        UpdatePostResponse response = postService.update(postId, loginUserId, request);
         return ResponseEntity.ok().body(ApiResponse.of("POST_MODIFIED", response));
     }
 
     @DeleteMapping("/posts/{postId}")
-    public ResponseEntity<?> deletePost(@PathVariable Integer postId) {
-        postService.deleteById(postId);
+    public ResponseEntity<?> deletePost(
+            @PathVariable Integer postId,
+            @AuthenticationPrincipal Integer loginUserId) {
+        postService.deleteById(postId,loginUserId);
         return ResponseEntity.noContent().build();
     }
 
